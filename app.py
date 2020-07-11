61c0569fcbfae5609b84d765b27d5e14258160d9
kappa224 <komal.mistry224@gmail.com>
2020-07-11 01:40:54 -0700

    done with inital layout

--- app.py
+++ app.py
@@ -8,21 +8,55 @@ import pandas as pd
 
 app = dash.Dash(__name__)
 
-# assume you have a "wide-form" data frame with no index
-# see https://plotly.com/python/wide-form/ for more options
+colors = {
+    'background': '#e0ffff',
+    'text': '#7FDBFF'
+}
+
 
 app.layout = html.Div([
-    html.H2('CHAINS OF RESEARCH'),
-    html.Img(src = '../assets/chains.png')
-], className = 'banner')
+    
+    # Title Banner
+    html.Div([
+        html.H2('CHAINS OF RESEARCH'),
+        html.Img(src = '../assets/chains.png')
+        ], className = 'banner'),
 
+    # Conference Selection Box
+    
+    html.Div([
+        
+        html.Content("""
+        Chains of Research is an online investigative and analytical tool built by parsing five popular ACM conferences which are particularly relevant for IoT research.
+        
+        
+        
+        """, style = {'color' : 'text'}),
+        
+        
+        
+        html.Label('Conference Name', style = {'color' : 'text'}),
+        dcc.Dropdown(id = 'conf_selection',
+        options = [
+                {'label': 'MobiCom', 'value' : 'mobicom'},
+                {'label' : 'MobiHoc', 'value': 'mobihoc'},
+                {'label' : 'SigComm', 'value': 'sigcomm'},
+                {'label' : 'IPSN', 'value': 'ipsn'},
+                {'label' : 'SenSys', 'value': 'sensys'},
+                    ],
+                value = 'mobicom'
+                        )
+    
+        
+        
+        ],style = {'columnCount' : 2})
+    
+])
+#e0ffff
 
-
-
-
-# app.css.append_css({
-#     "external_url":"https://codepen.io/chriddyp/pen/bWLwgP.css"
-# })
+app.css.append_css({
+    "external_url":"https://codepen.io/chriddyp/pen/bWLwgP.css"
+})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
