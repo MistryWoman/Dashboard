b6ff0fee62cb64955743c9d00f7eda9ac414a29f
kappa224 <komal.mistry224@gmail.com>
2020-07-13 22:55:30 -0700

    fixed dynamic dropdown return value

--- app3.py
+++ app3.py
@@ -19,24 +19,29 @@ sigcomm_df = pd.read_csv('data/Clean_Authors/sigcomm_authors.csv')
 
 sensys_df = pd.read_csv('data/Clean_Authors/sensys_authors.csv')
 
-def conf_years(conf_df):
-    "Given the df for a conference returns the list of years "
+def conf_years(conf, conf_df):
+    "Given the conf name and the dataframe for the same returns a list of the years for that conference "
     year_list = list(set(conf_df['Year']))
     year_list.sort()
-    return year_list
+    
+    new_year_list = []
+    for year in year_list:
+        year = str(conf) + "_" + str(year)
+        new_year_list.append(year)
+    
+    return new_year_list
 
 year_dict = collections.defaultdict(list)
 
-year_dict['ipsn'] = conf_years(ipsn_df)
+year_dict['ipsn'] = conf_years('ipsn', ipsn_df)
 
-year_dict['mobicom'] = conf_years(mobicom_df)
+year_dict['mobicom'] = conf_years('mobicom', mobicom_df)
 
-year_dict['mobihoc'] = conf_years(mobihoc_df)
+year_dict['mobihoc'] = conf_years('mobihoc', mobihoc_df)
 
-year_dict['sensys'] = conf_years(sensys_df)
+year_dict['sensys'] = conf_years('sensys', sensys_df)
 
-year_dict['sigcomm'] = conf_years(sigcomm_df)
-
+year_dict['sigcomm'] = conf_years('sigcomm', sigcomm_df)
 ################################################################3
 
 
@@ -45,20 +50,34 @@ nestedOptions = year_dict[years[0]]
 
 app.layout = html.Div(
     [
+        
+        # Title Banner
         html.Div([
-        dcc.Dropdown(
-            id='year-dropdown',
-            options=[{'label':year, 'value':year} for year in years],
-            value = list(year_dict.keys())[0]
-            ),
+            html.H2('CHAINS OF RESEARCH'),
+            html.Img(src = '../assets/chains.png')
+        ], className = 'banner'),
+        
+        html.Hr(),
+        
+        # Creating the conference and year selection dropdowns
+        html.Div([
+            dcc.Dropdown(
+                id='year-dropdown',
+                options=[{'label':year, 'value':year} for year in years],
+                value = list(year_dict.keys())[0]
+                ),
             ],style={'width': '20%', 'display': 'inline-block'}),
+        
         html.Div([
-        dcc.Dropdown(
-            id='opt-dropdown',
-            ),
+            dcc.Dropdown(
+                id='opt-dropdown',
+                ),
             ],style={'width': '20%', 'display': 'inline-block'}
         ),
-        html.Hr()
+        
+        
+        html.Hr(),
+        html.Div(id='display-selected')
         
     ]
 )
@@ -69,6 +88,33 @@ app.layout = html.Div(
 )
 def update_date_dropdown(year):
     return [{'label': i, 'value': i} for i in year_dict[year]]
+
+
+# @app.callback(
+#     [dash.dependencies.Output('top-five-tpc', 'figure'),
+#      dash.dependencies.Output('top-five-author', 'figure'),
+#      dash.dependencies.Output('middle-tpc', 'figure'),
+#      dash.dependencies.Output('middle-author', 'figure'),
+#      dash.dependencies.Output('last-five-tpc', 'figure'),
+#      dash.dependencies.Output('last-five-author', 'figure')
+#     ]
+#     [dash.dependencies.Input('opt-dropdown', 'value')])
+
+@app.callback(
+    dash.dependencies.Output('display-selected', 'children'),
+    [dash.dependencies.Input('opt-dropdown', 'value')])
+
+def set_display_children(selected_value):
+    return 'you have selected {} option'.format(selected_value)
+
+
+
+# def display_tpc_authors(selected_value):
+#     "Has to return all 6 figures"
+    
+    
+#     return 'you have selected {} option'.format(selected_value)
+
 
 
 
